6da591b3a5f9adbe7035409505c89e3b6e35a5df
oreoluwa-adebusoye <ogadebusoye@loyola.edu>
2025-11-25 18:42:10 -0500

    to allow merge

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         try{
-            String url = "";
+            String url = "jdbc:mysql://localhost:3306/test_db?user=root&password=";
             Connection con = DriverManager.getConnection(url);
         }catch(SQLException ex){
             System.out.println("Error in getting a DB Connection");
@@ -14,4 +14,4 @@ public class Main {
 
     }
     }
-}
+

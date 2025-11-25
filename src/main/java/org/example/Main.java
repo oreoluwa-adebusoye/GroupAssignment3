06c39c83de6bce526ca1d0d26aacdde764d8e807
take-the-hit-to-the-face <gli@loyola.edu>
2025-11-25 18:16:34 -0500

    changed connection in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,18 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        String ConUrl = "jdbc:mysql://localhost"; //protocol + url
+        String Port = "3306"; //default MySQL port
+        String Database = "groupassignment3"; // database/schema name
+        String Username = "root"; //read this from a local file
+        String Password = "your pass"; //Also read this from a file
+
         try{
-            String url = "";
-            Connection con = DriverManager.getConnection(url);
+            Connection con = DriverManager.getConnection(ConUrl+":"+Port+"/"+Database+ "?user="+Username+"&password="+Password);
+            con.close();
         }catch(SQLException ex){
-            System.out.println("Error in getting a DB Connection");
+            System.err.println(ex.getMessage());
         }
 
-    }
     }
 }

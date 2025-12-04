340522206891b2f33194d53c61fb62d465a64342
take-the-hit-to-the-face <gli@loyola.edu>
2025-12-04 17:20:27 -0500

    unversioned files for the dialog box

---
+++ src/main/java/cs485/assignment3/view/NewServiceDialog.java
@@ -0,0 +1,64 @@
+package cs485.assignment3.view;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class NewServiceDialog extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JTextField txtService;
+    private JTextField txtType;
+    private JTextField txtDescr;
+    private JTextField txtPrice;
+    private JTextField txtTime;
+
+    public NewServiceDialog() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onOK() {
+        System.out.println(txtService.getText());
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        NewServiceDialog dialog = new NewServiceDialog();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}

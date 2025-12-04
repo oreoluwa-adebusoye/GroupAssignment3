1d3731c1139ed972355d22b9e7cc85f0d4552087
oreoluwa-adebusoye <ogadebusoye@loyola.edu>
2025-12-04 16:18:46 -0500

    test starter

--- src/test/java/cs485/assignment3/model/dao/ServiceDAOTest.java
+++ src/test/java/cs485/assignment3/model/dao/ServiceDAOTest.java
@@ -1,5 +1,20 @@
 package cs485.assignment3.model.dao;
 import cs485.assignment3.model.entity.Service;
+import cs485.assignment3.model.dao.ServiceDAO;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 public class ServiceDAOTest {
+    @Test
+    public void testFake(){
+        assertAll(
+                () -> assertEquals(1,1)
+        );
+    }
+
+
 }

01b093a54315bcc8274912212e6c640e54323364
take-the-hit-to-the-face <gli@loyola.edu>
2025-12-04 17:15:31 -0500

    made dialog box with text fields

--- src/main/java/cs485/assignment3/view/MainFrame.java
+++ src/main/java/cs485/assignment3/view/MainFrame.java
@@ -1,4 +1,27 @@
 package cs485.assignment3.view;
 
-public class MainFrame {
+import javax.swing.*;
+
+
+public class MainFrame extends javax.swing.JFrame{
+
+    public MainFrame() {
+        super("Vet Clinic Service");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800,600);
+        createMenuBar();
+    }
+
+    public void createMenuBar(){
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu mnuFile = new JMenu("File");
+        JMenuItem mnuExit = new JMenuItem("Exit");
+        mnuExit.addActionListener(e -> { System.exit(0); });
+        mnuFile.add(mnuExit);
+
+        menuBar.add(mnuFile);
+        this.setJMenuBar(menuBar);
+
+    }
 }
